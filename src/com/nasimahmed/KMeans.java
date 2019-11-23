4a7bfc765297a5a53c7a9e075f20797443fb9d15
Nasim Ahmed <nasim@Nasims-MacBook-Pro.local>
2019-11-23 03:48:06 +0100

    added kmeans algo

--- src/com/nasimahmed/KMeans.java
+++ src/com/nasimahmed/KMeans.java
@@ -16,6 +16,8 @@ public class KMeans {
     public static void main(String[] args) {
         KMeans kMeans = new KMeans();
         kMeans.init();
+        kMeans.calculate();
+        kMeans.plotClusters();
     }
 
     public KMeans(){
@@ -60,7 +62,7 @@ public class KMeans {
            cluster.setCentroid(centroid);
            clusters.add(cluster);
         }
-       plotClusters();
+
     }
 
     public void plotClusters(){
@@ -69,6 +71,100 @@ public class KMeans {
             c.plotCluster();
         }
     }
+
+
+    public void calculate(){
+         boolean isFinish = false;
+         int iteration = 0;
+
+         while (!isFinish) {
+             clearClusters();
+
+             List lastCentroids = getCentroids();
+
+             //Assign points to the closer cluster
+             assignClusters();
+
+             //calculate new centroids
+             calculateCentroids();
+
+             iteration ++;
+
+             if (iteration == 3){
+                 isFinish = true;
+             }
+         }
+
+    }
+
+    // remove all the points from the cluster
+    public void clearClusters(){
+        for(Cluster cluster: clusters){
+            cluster.clear();
+        }
+    }
+
+    public List getCentroids(){
+        List centroids = new ArrayList(NUM_CLUSTERS);
+
+        for (Cluster cluster: clusters){
+            Point pt = cluster.getCentroid();
+            Point point = new Point(pt.getX(), pt.getY());
+            centroids.add(point);
+        }
+        return centroids;
+    }
+
+    public void assignClusters(){
+         double max = Double.MAX_VALUE;
+         double min = max;
+         int cluster = 0;
+         double distance = 0.0;
+
+         for(Point point: points){
+             min = max;
+
+             for(int i = 0; i < NUM_CLUSTERS; i++){
+                Cluster c = clusters.get(i);
+                distance = Point.distance(point, c.getCentroid());
+
+                if (distance < min) {
+                    min = distance;
+                    cluster = i;
+                }
+             }
+
+             point.setCluster_number(cluster);
+             clusters.get(cluster).addPoint(point);
+         }
+    }
+
+    public void calculateCentroids(){
+        for(Cluster cluster: clusters){
+            double sumX = 0.0;
+            double sumY = 0.0;
+
+            List<Point> list = cluster.getPoints();
+            int number_ofPoints = list.size();
+
+            for(Point pt: list){
+                sumX += pt.getX();
+                sumY += pt.getY();
+            }
+
+            Point centroid = cluster.getCentroid();
+
+            if (number_ofPoints > 0){
+                double newX = sumX / number_ofPoints;
+                double newY = sumY / number_ofPoints;
+                centroid.setX(newX);
+                centroid.setY(newY);
+            }
+
+        }
+    }
+
+
 
 
 }
